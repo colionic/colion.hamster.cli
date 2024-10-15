7d626825eb39a51dac1e790d1876e7463a694b8b
xujiantong <xujiantong.sd@chinatelecom.cn>
2024-10-15 17:09:44 +0800

    feat: 脚手架框架初始化

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+const commander = require('commander')
+const path = require("node:path");
+const program = new commander.Command()
+const pkg = require('../package.json')
+global.__rootdir = path.join(__dirname,"../")
+
+program.name(Object.keys(pkg.bin)[0])
+    .usage("<command> [options]")
+    .helpOption('-h, --help', '显示帮助信息')
+    .version(pkg.version, '-v, --version', '显示当前版本号')
+    .option('-d --debug', '启用调试模式');
+
+program.parse(process.argv)
