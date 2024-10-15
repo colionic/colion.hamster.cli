dc665597d1ad1fdd23fa2587f9c7066db6b91782
xujiantong <xujiantong.sd@chinatelecom.cn>
2024-10-15 18:44:59 +0800

    feat: 实现选择并打开常用网站的命令行工具

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
-const commander = require('commander')
-const path = require("node:path");
-const program = new commander.Command()
-const pkg = require('../package.json')
-global.__rootdir = path.join(__dirname,"../")
+const commander = require('commander');
+const path = require('node:path');
+const program = new commander.Command();
+const pkg = require('../package.json');
+const init = require('../lib/init');
+global.__rootdir = path.join(__dirname, '../');
 
-program.name(Object.keys(pkg.bin)[0])
-    .usage("<command> [options]")
-    .helpOption('-h, --help', '显示帮助信息')
-    .version(pkg.version, '-v, --version', '显示当前版本号')
-    .option('-d --debug', '启用调试模式');
+program
+  .name(Object.keys(pkg.bin)[0])
+  .usage('<command> [options]')
+  .helpOption('-h, --help', '显示帮助信息')
+  .version(pkg.version, '-v, --version', '显示当前版本号')
+  .option('-d --debug', '启用调试模式');
 
-program.parse(process.argv)
+init();
+
+program.parse(process.argv);
