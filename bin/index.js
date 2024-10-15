9eb7d409bcedfaf73b79a33af614a857ce2364e3
xujiantong <xujiantong.sd@chinatelecom.cn>
2024-10-15 19:39:42 +0800

    feat: 增加版本校验功能

--- bin/index.js
+++ bin/index.js
@@ -4,17 +4,31 @@ const path = require('node:path');
 const program = new commander.Command();
 const pkg = require('../package.json');
 const init = require('../lib/init');
-(async () => {})();
-
 global.__rootdir = path.join(__dirname, '../');
 
-program
-  .name(Object.keys(pkg.bin)[0])
-  .usage('<command> [options]')
-  .helpOption('-h, --help', '显示帮助信息')
-  .version(pkg.version, '-v, --version', '显示当前版本号')
-  .option('-d --debug', '启用调试模式');
+(async () => {
+  const response = await fetch('https://registry.npmjs.org/@colion/hamster');
+  const latest = await response.json();
+  const latestVersion = latest['dist-tags'].latest;
+  console.log(latestVersion);
 
-init();
+  console.clear();
+  if (pkg.version !== latestVersion) {
+    console.log(
+      `当前版本${pkg.version}，最新版本${latestVersion}，请更新 npm install @colion/hamster@${latestVersion}`,
+    );
+  }
+  start();
+})();
 
-program.parse(process.argv);
+function start() {
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [options]')
+    .helpOption('-h, --help', '显示帮助信息')
+    .version(pkg.version, '-v, --version', '显示当前版本号')
+    .option('-d --debug', '启用调试模式');
+
+  init();
+  program.parse(process.argv);
+}
